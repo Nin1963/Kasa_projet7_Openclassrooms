476fa088690e49c5bd352df8a7a776625f7378c3
Nin1963 <ninayamina1963@gmail.com>
2022-10-20 09:25:27 +0200

    Update css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './pages/Home/Home'
 import Housing from './pages/Housing/Housing'
 import Error from './pages/Error/Error'
 import About from './pages/About/About';
-import './App.css';
 import Header from './components/Header/Header';
 import'./components/Header/Header.css'
 import Footer from './components/Footer/Footer';
@@ -12,16 +11,14 @@ import './components/Footer/Footer.css'
 function App() {
   return (
     <Router>
-      <div className='home'>
         <Header />
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/housing" element={<Housing />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/housing" element={<Housing />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
-        </div>
     </Router>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,15 @@
-import '../../App.css'
 import bannerHome from '../../assets/img/bannerHome.png'
+import bannerMobile from '../../assets/img/bannerMobile.png'
 import '../../components/Banner/Banner.css'
 import Card from '../../components/Card/Card'
 import '../../components/Card/Card.css'
 
 function Home() {
     return (
-        <div classname="product-section">
+        <div className="product-section">
             <div className="banner banner-home">
                 <img className="img-banner" src={bannerHome} alt="Paysage" />
+                <img className="img-banner-mobile" src={bannerMobile} alt="Paysage" />
                 <p className="name-home">Chez vous, partout et ailleurs</p>
             </div>
             <div className="card-section">
